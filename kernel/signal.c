504a452cb509cb7a78ceac67823cea7e1ee0c7f2
HanGuangKeJi <liuyan6661@gmail.com>
2025-09-18 21:28:28 +0800

    Update signal.c

--- kernel/signal.c
+++ kernel/signal.c
@@ -1291,7 +1291,7 @@ int do_send_sig_info(int sig, struct siginfo *info, struct task_struct *p,
 
 	if (start_rekernel_server() == 0) {
  		if (line_is_frozen(p) && (sig == SIGKILL || sig == SIGTERM || sig == SIGABRT || sig == SIGQUIT)) {
-     		char binder_kmsg[REKERNEL_PACKET_SIZE];
+     		char binder_kmsg[PACKET_SIZE];
      		snprintf(binder_kmsg, sizeof(binder_kmsg), "type=Signal,signal=%d,killer=%d,dst=%d;", sig, task_uid(p).val, task_uid(current).val);
      		send_netlink_message(binder_kmsg, strlen(binder_kmsg));
  		}
